633218b4fa980b86cb6625132909ed502e3d365f
Davi Bispo <davibispo2004@hotmail.com>
2025-04-13 22:07:17 -0300

    feat: added header and search field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { FaGithub } from 'react-icons/fa6';
+import { FaSearch } from 'react-icons/fa';
 
 function App() {
+  const [username, setUsername] = useState('')
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    if (!username) return;
+  }  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app'>
+      <header className='header'>
+        <FaGithub size={48}/>
+        <h1>
+          Perfil <strong>GitHub</strong>
+        </h1>
       </header>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Digite um usuário do Github'
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button>
+          <FaSearch size={18}/>
+        </button>
+
+      </form>
     </div>
   );
 }
 
 export default App;
+
